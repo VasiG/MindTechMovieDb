386949674e366b6842416fb50bf7bb18514c499a
VasiG <vasi.geri@gmail.com>
2024-09-12 17:38:35 +0200

    Add project files.

---
+++ MindTechMovieDbProject/TBDbApiService.cs
@@ -0,0 +1,13 @@
+﻿namespace MindTechMovieDbProject
+{
+    public class TBDbApiService
+    {
+        private readonly HttpClient httpClient;
+        private readonly string apiKey = "359c412e72a55bc597abb258b101d786";
+
+        public TBDbApiService()
+        {
+            
+        }
+    }
+}
